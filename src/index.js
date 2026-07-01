13ad19929fe454c76567cc15d0d7134ef85c38b0
Abdul Ghafoor <jamghafoor1122@gmail.com>
2026-07-01 20:31:48 +0500

    feat: configure PostgreSQL connection pool and add database health check endpoint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-
+import pool from "./config/db.js";
 dotenv.config();
 
 const app = express();
@@ -19,7 +19,16 @@ app.use((err, req, res, next) => {
   res.status(500).json({ error: "Internal server error" });
 });
 
+//BASE URL
+app.get("/", async (req, res) => {
+  const result = await pool.query("SELECT current_database()");
+  res.json({
+    message: "API is running",
+    database: result.rows[0].current_database,
+  });
+});
+
 // Start Server
-app.listen(process.env.PORT || 5000, () => {
+app.listen(process.env.PORT || 5000, async () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
 });
