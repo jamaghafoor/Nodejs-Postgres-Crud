1400a5b897cb09928ee09ef36fa0e44803fc227b
Abdul Ghafoor <jamghafoor1122@gmail.com>
2026-07-07 14:45:33 +0500

    Added Models and Controllers for users

---
+++ src/controllers/usersController.js
@@ -0,0 +1,62 @@
+import { handleResponse } from "../middlewares/responseHandler";
+import {
+  createNewUserService,
+  deleteUserService,
+  getAllUsersService,
+  getUserByIdService,
+  updateUserService,
+} from "../models/userModels";
+
+export const getAllUserController = async (req, res, next) => {
+  try {
+    const users = await getAllUsersService();
+    handleResponse(res, 200, "Retrieved all users.", users);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUserByIdController = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const user = await getUserByIdService(id);
+    if (!user) return handleResponse(res, 404, "No user found against this id");
+    handleResponse(res, 200, "user fetched successfully", user);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const createUserController = async (req, res, next) => {
+  const { name, email } = req.body;
+  try {
+    const newUser = await createNewUserService(name, email);
+    handleResponse(res, 201, "New user created successfully.", newUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateUserController = async (req, res, next) => {
+  const { name, email } = req.body;
+  const { id } = req.params;
+  try {
+    const updatedUser = await updateUserService(id, name, email);
+    if (!updatedUser)
+      return handleResponse(res, 404, "No user found against this id");
+    handleResponse(res, 200, "User updated successfully.", updatedUser);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const deleteUserController = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await deleteUserService(id);
+    if (!deletedUser)
+      return handleResponse(res, 404, "No user found against this id");
+
+    handleResponse(res, 200, "User deleted successfull.", deletedUser);
+  } catch (error) {}
+};
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,17 @@
 import express from "express";
 const router = express.Router();
+import {
+  getAllUserController,
+  getUserByIdController,
+  createUserController,
+  updateUserController,
+  deleteUserController,
+} from "../controllers/usersController";
 
-router.get("/", (async) => ({}));
-router.get("/:id", (async) => ({}));
-router.post("/", (async) => ({}));
-router.put("/:id", (async) => ({}));
-router.delete("/:id", (async) => ({}));
+router.get("/", getAllUserController);
+router.get("/:id", getUserByIdController);
+router.post("/", createUserController);
+router.put("/:id", updateUserController);
+router.delete("/:id", deleteUserController);
 
 export default router;
