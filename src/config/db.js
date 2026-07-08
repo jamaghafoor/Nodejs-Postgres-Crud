6d09bc3e9677cdfa3c22d1326254f13b237ad970
Abdul Ghafoor <jamghafoor1122@gmail.com>
2026-07-08 13:32:18 +0500

    refactor: update database configuration to use standard PG environment variables and support connection strings

--- src/config/db.js
+++ src/config/db.js
@@ -6,11 +6,12 @@ dotenv.config();
 const { Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_PUBLIC_URL || process.env.DATABASE_URL,
 });
 
 pool.on("connect", () => {
